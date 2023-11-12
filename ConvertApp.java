ee7b1b4f46f7e52a4bc0dac9aeba66d223ca2549
tfchowdhury <127281818+tfchowdhury@users.noreply.github.com>
2023-11-12 07:17:00 -0600

    Add files via upload

---
+++ ConvertApp.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ConvertApp {
+
+    public static void main(String[] args) {
+        ConvertFrame convertFrame = new ConvertFrame();
+        convertFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        convertFrame.setSize(400, 400);
+        convertFrame.setVisible(true);
+    }
+}
